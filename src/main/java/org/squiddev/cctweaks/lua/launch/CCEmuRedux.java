2c76bd8f4c287f643f568146f65bf6a1244c9f3e
SquidDev <bonzoweb@hotmail.co.uk>
2016-06-08 15:17:22 +0100

    Add recolouring support for CCEmuRedux

--- src/main/java/org/squiddev/cctweaks/lua/launch/CCEmuRedux.java
+++ src/main/java/org/squiddev/cctweaks/lua/launch/CCEmuRedux.java
@@ -2,15 +2,12 @@ package org.squiddev.cctweaks.lua.launch;
 
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.tree.InsnList;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.VarInsnNode;
+import org.objectweb.asm.tree.*;
 import org.squiddev.luaj.luajc.utils.AsmUtils;
 import org.squiddev.patcher.transformer.IPatcher;
 import org.squiddev.patcher.visitors.FindingVisitor;
 
-import static org.objectweb.asm.Opcodes.ALOAD;
-import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
+import static org.objectweb.asm.Opcodes.*;
 import static org.squiddev.cctweaks.lua.launch.Launcher.*;
 
 /**
@@ -21,12 +18,23 @@ public class CCEmuRedux {
 		Integer width = parseNumber("cctweaks.ccemu.width");
 		Integer height = parseNumber("cctweaks.ccemu.height");
 
+		Integer back = parseHexColor("cctweaks.ccemu.color");
+
 		RewritingLoader loader = setupLoader();
 
 		if (width != null || height != null) {
-			loader.chain.add(new CCEmuPatcher(
+			loader.chain.add(new SizePatcher(
 				width == null ? 51 : width,
 				height == null ? 19 : height
+			));
+		}
+
+		if (back != null) {
+			int val = back;
+			loader.chain.add(new ColorPatcher(
+				(val >> (8 * 2) & 255) / 255.0f,
+				(val >> (8 * 1) & 255) / 255.0f,
+				(val >> (8 * 0) & 255) / 255.0f
 			));
 		}
 
@@ -34,12 +42,33 @@ public class CCEmuRedux {
 		execute(loader, "com.xtansia.ccemu.desktop.DesktopLauncher", new String[0]);
 	}
 
+	private static Integer parseHexColor(String key) {
+		String value = System.getProperty(key);
+		if (value == null) return null;
 
-	public static class CCEmuPatcher implements IPatcher {
+		if (value.charAt(0) == '#') value = value.substring(1);
+
+		if (value.length() == 3) {
+			value = "" +
+				value.charAt(0) + value.charAt(0) +
+				value.charAt(1) + value.charAt(1) +
+				value.charAt(2) + value.charAt(2);
+		} else if (value.length() != 6) {
+			throw new NumberFormatException("Cannot parse " + key + ": " + "expected string of length 3 or 6, got " + value.length());
+		}
+
+		try {
+			return Integer.valueOf(value, 16);
+		} catch (NumberFormatException e) {
+			throw new NumberFormatException("Cannot parse " + key + ": " + e.getMessage());
+		}
+	}
+
+	private static class SizePatcher implements IPatcher {
 		private final int width;
 		private final int height;
 
-		public CCEmuPatcher(int width, int height) {
+		public SizePatcher(int width, int height) {
 			this.width = width;
 			this.height = height;
 		}
@@ -63,6 +92,43 @@ public class CCEmuRedux {
 					AsmUtils.constantOpcode(visitor, height);
 				}
 			}.onMethod("<init>").once().mustFind();
+		}
+	}
+
+	private static class ColorPatcher implements IPatcher {
+		private final float r;
+		private final float g;
+		private final float b;
+
+		private ColorPatcher(float r, float g, float b) {
+			this.r = r;
+			this.g = g;
+			this.b = b;
+		}
+
+		@Override
+		public boolean matches(String className) {
+			return className.equals("com.xtansia.ccemu.CCEmulator");
+		}
+
+		@Override
+		public ClassVisitor patch(String className, ClassVisitor delegate) throws Exception {
+			return new FindingVisitor(delegate,
+				new LdcInsnNode(0.87f),
+				new LdcInsnNode(0.87f),
+				new LdcInsnNode(0.87f),
+				new InsnNode(FCONST_1),
+				new MethodInsnNode(INVOKEINTERFACE, "com/badlogic/gdx/graphics/GLCommon", "glClearColor", "(FFFF)V", true)
+			) {
+				@Override
+				public void handle(InsnList nodes, MethodVisitor visitor) {
+					visitor.visitLdcInsn(r);
+					visitor.visitLdcInsn(g);
+					visitor.visitLdcInsn(b);
+					visitor.visitInsn(FCONST_1);
+					visitor.visitMethodInsn(INVOKEINTERFACE, "com/badlogic/gdx/graphics/GLCommon", "glClearColor", "(FFFF)V", true);
+				}
+			}.onMethod("draw").once().mustFind();
 		}
 	}
 
